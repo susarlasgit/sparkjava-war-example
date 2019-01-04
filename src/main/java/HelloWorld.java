2f94b55fab577cd370b106727cb7f31f916f166f
susarlasgit <45976118+susarlasgit@users.noreply.github.com>
2019-01-04 14:46:20 -0600

    update 3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello dear World 2");
+		get("/hello", (req, res) -> "Hello dear World 3");
 	}
 }
